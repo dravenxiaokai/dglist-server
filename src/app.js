ee518a20e38985dd720a0fef462b61ae31f5b2fe
Draven <jspankai@vip.qq.com>
2017-08-13 15:05:16 +0800

    create an express server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hello, Express!');
+});
+
+app.listen(3000, 'localhost', () => {
+    console.log('Server is listening on port 3000');
+});
